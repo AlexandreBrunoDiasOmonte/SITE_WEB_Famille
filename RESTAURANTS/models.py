c53dff7650369afff31088f5846d91f55f384bb7
Alexandre Dias- -Omonte <alexandrebruno.dias--omonte@orange.fr>
2020-02-28 15:50:17 +0100

    ajout de la BDD

--- RESTAURANTS/models.py
+++ RESTAURANTS/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import re
 
-# Create your models here.
+
+class Restaurants(models.Model):
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    website = models.URLField()
+
+    def __str__(self):
+        return {self.name, self.adress, self.phone, self.website}
