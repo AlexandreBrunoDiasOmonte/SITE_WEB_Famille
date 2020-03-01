06ef7ebb94a0818e18d163d28ccf258dae6d5dfc
Alexandre Dias- -Omonte <alexandrebruno.dias--omonte@orange.fr>
2020-03-01 20:36:41 +0100

    ajout de la ville dans la BDD

--- RESTAURANTS/models.py
+++ RESTAURANTS/models.py
@@ -4,9 +4,11 @@ import re
 
 class Restaurants(models.Model):
     name = models.CharField(max_length=100)
-    adress = models.CharField(max_length=200)
+    email = models.EmailField(max_length=150, default="name@exemple.com")
+    adresse = models.CharField(max_length=200)
+    ville = models.CharField(max_length=200)
     phone = models.CharField(max_length=10)
     website = models.URLField()
 
     def __str__(self):
-        return {self.name, self.adress, self.phone, self.website}
+        return {self.name, self.email, self.phone, self.website}
