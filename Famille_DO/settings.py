7a3e8c064b6efcca478844f34d7835323d2e871e
Alexandre Dias- -Omonte <alexandrebruno.dias--omonte@orange.fr>
2020-03-03 16:45:31 +0100

    affichage des restaurants

--- Famille_DO/settings.py
+++ Famille_DO/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media/'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/'),
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media/'),
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
--- RESTAURANTS/models.py
+++ RESTAURANTS/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Restaurant(models.Model):
-    name = models.CharField('Nom', max_length=100)
-    email = models.EmailField(max_length=150, null=True)
-    adresse = models.CharField(max_length=200)
-    codePostal = models.BigIntegerField()
-    ville = models.CharField(max_length=200, default="Mulhouse")
-    phone = models.CharField('téléphone', max_length=12)
-    website = models.URLField('site web')
+    name = models.CharField('Nom', max_length=100, blank=False)
+    email = models.EmailField(max_length=150, blank=True)
+    adresse = models.CharField(max_length=200, blank=False)
+    codePostal = models.BigIntegerField(default=68100, blank=False)
+    ville = models.CharField(max_length=200, default="Mulhouse", blank=False)
+    phone = models.CharField('téléphone', max_length=12, blank=True)
+    website = models.URLField('site web', blank=True)
     available = models.BooleanField('disponnible', default=True)
-    picture = models.URLField('photo')
+    image = models.URLField("image", blank=True)
 
     def __str__(self):
         return self.name
