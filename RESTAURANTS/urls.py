c7a018aeeffa62790bc1f09b2fc9839c4fa31f18
Alexandre Dias- -Omonte <alexandrebruno.dias--omonte@orange.fr>
2020-03-06 14:27:40 +0100

    ajout champ de recherche

--- RESTAURANTS/urls.py
+++ RESTAURANTS/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url, include
+#from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'RESTAURANTS'
 urlpatterns = [
-    url(r'', views.restaurants, name='restaurants'),
+    path('', views.restaurants, name='restaurants'),
+    path('details/<int:resto_id>/', views.details, name='details'),
+    path('search/', views.recherche, name='recherche'),
 ]
--- RESTAURANTS/views.py
+++ RESTAURANTS/views.py
@@ -4,6 +4,28 @@ from .models import Restaurant
 
 def restaurants(request):
     restos = Restaurant.objects.filter(available=True)
-   # liste = "\n".join(formated_restos)
     context = {'restos': restos}
     return render(request, 'RESTAURANTS/restaurants.html', context)
+
+
+def details(request, resto_id):
+    resto = Restaurant.objects.get(pk=resto_id)
+    context = {'resto': resto}
+    return render(request, 'RESTAURANTS/details.html', context)
+
+
+def recherche(request):
+    query = request.GET.get('query')
+    if not query:
+        restos = Restaurant.objects.all()
+    else:
+        # title contains the query is and query is not sensitive to case.
+        restos = Restaurant.objects.filter(name__icontains=query)
+    if not restos.exists():
+        restos = Restaurant.objects.filter(ville__icontains=query)
+    title = "Résultats pour la requête < %s >" % query
+    context = {
+        'restos': restos,
+        'title': title
+    }
+    return render(request, 'RESTAURANTS/recherche.html', context)
