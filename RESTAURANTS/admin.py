508ea0a2436548fd3d7aa3c0692940aea782a4e3
Alexandre Dias- -Omonte <alexandrebruno.dias--omonte@orange.fr>
2020-03-13 14:37:46 +0100

    modification de la partie admin

--- RESTAURANTS/admin.py
+++ RESTAURANTS/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from .models import Restaurant
 
 
-# admin.site.register(Restaurant)
-
-
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'ville']
+    search_fields = ['name', 'ville', 'codePostal']
     list_filter = ['ville']
+    list_display = ['name', 'adresse', 'ville', 'phone', 'website']
+    ordering = ['name']
+    list_per_page = 10
+
+
